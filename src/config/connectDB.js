a87a252b85f3a0b9f4567f4c2e6b6daf674b8e5d
Henry Nguyen <nkhuy@humaxdigital.com>
2025-05-14 13:31:32 +0700

    feat: test jenkins

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -15,6 +15,8 @@ const sequelize = new Sequelize(
     collate: 'utf8_general_ci',
     timezone: '+07:00'
   });
+// Test the connection
+// sequelize.sync({ force: true }) // Use this line to drop and recreate the database schema
 
 (async () => {
   try {
